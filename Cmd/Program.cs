9312fd2ffc8392522461881504e13b06e7d45e8b
Guido Nieuwhof <guido.nieuwhof@gmail.com>
2018-01-13 01:39:52 +0100

    Added convert from bytes logic.

--- Cmd/Program.cs
+++ Cmd/Program.cs
@@ -1,15 +1,25 @@
 ﻿namespace Cmd
 {
+    using Hexadecimal;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     class Program
     {
         static void Main(string[] args)
         {
+            var guid = Guid.NewGuid();
+
+            string lower = HexConverter.FromBytes(guid.ToByteArray());
+            string upper = HexConverter.FromBytes(guid.ToByteArray(), uppercase: true);
+
+            if (lower.ToUpper() != upper)
+            {
+                ;
+                throw new Exception();
+            }
+
+            Console.WriteLine("OK");
+            Console.ReadKey();
         }
     }
 }
---
+++ Hexadecimal/HexConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace Hexadecimal
+{
+    public class HexConverter
+    {
+        public static string FromBytes(byte[] bytes, bool uppercase = false)
+        {
+            // Every byte will be converted to two chars in the result.
+            var result = new char[bytes.Length * 2];
+
+            for (int i = 0; i < bytes.Length; ++i)
+            {
+                // Get the byte.
+                uint value = uppercase ?
+                    Lookup.Uppercase[bytes[i]] :
+                    Lookup.Lowercase[bytes[i]];
+
+                // Set the HEX chars.
+                int doubleI = (i * 2);
+                result[doubleI] = (char)value;
+                result[++doubleI] = (char)(value >> 16);
+            }
+
+            return new string(result);
+        }
+    }
+}
